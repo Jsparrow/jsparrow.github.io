975fa9f55331b0b967d7926321ef9973c8e63ade
Martin Huter <martin.huter@splendit.at>
2018-09-26 15:10:02 +0200

    UPDATE docs

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 #if you are deploying to https://jsparrow11.github.io
-git push -f git@github.com:Jsparrow11/jsparrow11.github.io.git master
+git push -f git@github.com:Jsparrow/jsparrow.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
+
+rm -rf docs/.vuepress/dist
