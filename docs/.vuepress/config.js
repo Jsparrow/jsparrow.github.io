7fd7618a5c66b9c17999da7444dae9ba21a741da
Martin Huter <martin.huter@splendit.at>
2018-12-12 16:08:28 +0100

    Revert "SIM-1382 ADD other logo"
    This reverts commit e39f3af3b32580b142543b03f1d6962bc1373e20.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'Documentation',
+    title: 'jSparrow Documentation',
     head: [
         ['link', {
             rel: 'icon',
@@ -28,7 +28,7 @@ module.exports = {
                 link: '/rules/'
             }
         ],
-        logo: '/jsparrow.png',
+        logo: '/logo.png',
         sidebar: {
             '/maven/': ['getting-started', 'release-notes'],
             '/eclipse/': ['getting-started', 'installation-guide', 'additional-configuration', 'release-notes'],
