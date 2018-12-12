e6b799232ce31401d3f3fdff66a55bef629e25e3
Martin Huter <martin.huter@splendit.at>
2018-12-12 15:52:16 +0100

    SIM-1382 UNDO navbar changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,43 +13,15 @@ module.exports = {
             },
             {
                 text: 'Maven Plugin',
-                items: [{
-                    text: 'Getting Started',
-                    link: '/maven/getting-started.html'
-                }]
+                link: '/maven/getting-started.html'
             },
             {
                 text: 'Eclipse Plugin',
                 link: '/eclipse/getting-started.html',
-                items: [{
-                        text: 'Getting Started',
-                        link: '/eclipse/getting-started.html'
-                    },
-                    {
-                        text: 'Installation Guide',
-                        link: '/eclipse/installation-guide.html'
-                    },
-                    {
-                        text: 'Additional Configuration',
-                        link: '/eclipse/additional-configuration.html'
-                    },
-                    {
-                        text: 'Release Notes',
-                        link: '/eclipse/release-notes.html'
-                    }
-                ]
             },
             {
                 text: 'GitHub App',
-                items: [{
-                        text: 'Getting Started',
-                        link: '/github/getting-started.html',
-                    },
-                    {
-                        text: 'Statistics',
-                        link: '/github/statistics.html',
-                    },
-                ]
+                link: '/github/getting-started.html'
             },
             {
                 text: 'Rules',
