dea586e5ea9298111290d0736caeb86c32a3cfc8
ardit.ymeri <ardit.ymeri@splendit.at>
2018-12-20 11:28:54 +0100

    SIM-1382 UPDATE separating tags with commas

--- docs/.vuepress/components/TagListShort.vue
+++ docs/.vuepress/components/TagListShort.vue
@@ -1,9 +1,13 @@
 <template lang="html">
   <div>
-    <span v-for="tag in Object.keys(tags)">
-        <router-link :to="{ path: `/tags.html#${tag}`}">{{tag}}</router-link>
-        &nbsp;
-    </span>
+    <p>
+      <router-link
+        v-for="(tag, index) in Object.keys(tags)"
+        :key="tag"
+        :to="{ path: `/tags.html#${tag}`}">
+        {{tag}}<span v-if="index+1 < Object.keys(tags).length">, </span>
+      </router-link>
+    </p>
   </div>
 </template>
 
@@ -11,19 +15,19 @@
 export default {
   computed: {
     tags() {
-      let tags = {}
+      let tags = {};
       for (let page of this.$site.pages) {
         for (let index in page.frontmatter.tags) {
-          const tag = page.frontmatter.tags[index]
+          const tag = page.frontmatter.tags[index];
           if (tag in tags) {
-            tags[tag].push(page)
+            tags[tag].push(page);
           } else {
-            tags[tag] = [page]
+            tags[tag] = [page];
           }
         }
       }
-      return tags
+      return tags;
     }
   }
-}
+};
 </script>
