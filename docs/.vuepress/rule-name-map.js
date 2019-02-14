6f09e113cf6156efd60aaf6658c4de291acb16b0
Martin Huter <martin.huter@splendit.at>
2019-02-14 13:46:46 +0100

    SIM-1439 UPDATE names from pages

--- docs/.vuepress/rule-name-map.js
+++ docs/.vuepress/rule-name-map.js
@@ -1,195 +1,214 @@
-module.exports =
-    {
-        "ArithmethicAssignment": {
-            "name": "Replace Assignment with Compound Operator",
-            "url": "rules/replace-assignment-with-compound-operator.html"
-        },
-        "BracketsToControl": {
-            "name": "Add Parenthesis to Control Statements",
-            "url": "rules/brackets-to-control.html"
-        },
-        "CodeFormatter": {
-            "name": "Reformat Code",
-            "url": "rules/code-formatter.html"
-        },
-        "DateDeprecated": {
-            "name": "Remove Deprecated Date Constructs",
-            "url": "rules/remove-deprecated-date-constructs.html"
-        },
-        "DiamondOperator": {
-            "name": "Remove Explicit Type Argument",
-            "url": "rules/remove-explicit-type-argument.html"
-        },
-        "EnhancedForLoopToStreamAnyMatch": {
-            "name": "Replace For-Loop with Stream::anyMatch",
-            "url": "rules/replace-for-loop-with-stream-any-match.html"
-        },
-        "EnhancedForLoopToStreamFindFirst": {
-            "name": "Replace For-Loop with Stream::findFirst",
-            "url": "rules/replace-for-loop-with-stream-findFirst.html"
-        },
-        "EnhancedForLoopToStreamForEach": {
-            "name": "Replace For-Loop with Stream::forEach",
-            "url": "rules/replace-for-loop-with-stream-for-each.html"
-        },
-        "EnhancedForLoopToStreamSum": {
-            "name": "Replace For-Loop with Stream::sum",
-            "url": "rules/replace-for-loop-with-stream-sum.html"
-        },
-        "EnumsWithoutEquals": {
-            "name": "Replace equals() on Enum Constants",
-            "url": "rules/replace-equals-on-enum-constants.html"
-        },
-        "FieldRenaming": {
-            "name": "Rename fields",
-            "url": "rules/rename-fields.html"
-        },
-        "FlatMapInsteadOfNestedLoops": {
-            "name": "Replace Nested Loops with flatMap",
-            "url": "rules/replace-nested-loops-with-flat-map.html"
-        },
-        "ForToForEach": {
-            "name": "Replace For-Loop with Enhanced-For-Loop",
-            "url": "rules/for-to-for-each.html"
-        },
-        "FunctionalInterface": {
-            "name": "Use Functional Interfaces",
-            "url": "rules/use-functional-interfaces.html"
-        },
-        "GuardCondition": {
-            "name": "Use Guard Condition",
-            "url": "rules/guard-condition.html"
-        },
-        "ImmutableStaticFinalCollections": {
-            "name": "Replace static final Collections with Collections.unmodifiable...()",
-            "url": "rules/replace-static-final-collections-with-collections-unmodifiable.html"
-        },
-        "IndexOfToContains": {
-            "name": "Replace indexOf() with contains()",
-            "url": "rules/replace-index-of-with-contains.html"
-        },
-        "InefficientConstructor": {
-            "name": "Replace Inefficient Constructors with valueOf()",
-            "url": "rules/replace-inefficient-constructors-with-value-of.html"
-        },
-        "LambdaForEachCollect": {
-            "name": "Use Stream::collect",
-            "url": "rules/use-stream-collect.html"
-        },
-        "LambdaForEachIfWrapperToFilter": {
-            "name": "Use Stream::filter",
-            "url": "rules/use-stream-filter.html"
-        },
-        "LambdaForEachMap": {
-            "name": "Use Stream::map",
-            "url": "rules/use-stream-map.html"
-        },
-        "LambdaToMethodReference": {
-            "name": "Replace Expression Lambda with Method Reference",
-            "url": "rules/replace-expression-lambda-with-method-reference.html"
-        },
-        "MultiCatch": {
-            "name": "Use Multi-Catch",
-            "url": "rules/use-multi-catch.html"
-        },
-        "MultiVariableDeclarationLine": {
-            "name": "Split Multiple Variable Declarations",
-            "url": "rules/split-multiple-variable-declarations.html"
-        },
-        "OptionalIfPresent": {
-            "name": "Use Optional::ifPresent",
-            "url": "rules/use-optional-if-present.html"
-        },
-        "OverrideAnnotation": {
-            "name": "Use @Override Annotation",
-            "url": "rules/override-annotation.html"
-        },
-        "PrimitiveBoxedForString": {
-            "name": "Remove Boxing for String Conversions",
-            "url": "rules/remove-boxing-for-string-conversions.html"
-        },
-        "PrimitiveObjectUseEquals": {
-            "name": "Use equals() on Primitive Objects",
-            "url": "rules/use-equals-on-primitive-objects.html"
-        },
-        "PutIfAbsent": {
-            "name": "Replace put(..) with putIfAbsent(..)",
-            "url": "rules/replace-put-with-put-if-absent.html"
-        },
-        "RearrangeClassMembers": {
-            "name": "Rearrange Class Members",
-            "url": "rules/rearrange-class-members.html"
-        },
-        "ReImplementingInterface": {
-            "name": "Remove Inherited Interfaces from Class Declaration",
-            "url": "rules/remove-inherited-interfaces-from-class-declaration.html"
-        },
-        "RemoveEmptyStatement": {
-            "name": "Remove Unnecessary Semicolons",
-            "url": "rules/remove-unnecessary-semicolons.html"
-        },
-        "RemoveExplicitCallToSuper": {
-            "name": "Remove Explicit Call To super()",
-            "url": "rules/remove-explicit-call-to-super.html"
-        },
-        "RemoveNewStringConstructor": {
-            "name": "Use String Literals",
-            "url": "rules/remove-new-string-constructor.html"
-        },
-        "RemoveToStringOnString": {
-            "name": "Remove toString() on String",
-            "url": "rules/remove-to-string-on-string.html"
-        },
-        "RemoveUnnecessaryThrows": {
-            "name": "Remove Unnecessary Thrown Exceptions on Method Signatures",
-            "url": "rules/remove-unnecessary-thrown-exceptions-on-method-signatures.html"
-        },
-        "StandardLogger": {
-            "name": "System Out To Logging",
-            "url": "rules/standard-logger.html"
-        },
-        "StatementLambdaToExpression": {
-            "name": "Remove Lambda Expression Parenthesis",
-            "url": "rules/remove-lambda-expression-parenthesis.html"
-        },
-        "StringBufferToBuilder": {
-            "name": "StringBuffer() to StringBuilder()",
-            "url": "rules/stringbuffer-to-stringbuilder.html"
-        },
-        "StringBuildingLoop": {
-            "name": "Replace For-Loop with Stream::collect(Collectors.joining())",
-            "url": "rules/replace-for-loop-with-stream-collect.html"
-        },
-        "StringConcatToPlus": {
-            "name": "Replace Concatenation with Infix-Operation",
-            "url": "rules/replace-concatenation-with-Infix-operation.html"
-        },
-        "StringFormatLineSeparator": {
-            "name": "Use Portable Newline",
-            "url": "rules/string-format-line-separator.html"
-        },
-        "StringLiteralEqualityCheck": {
-            "name": "Reorder String Equality Check",
-            "url": "rules/reorder-string-equality-check.html"
-        },
-        "StringUtils": {
-            "name": "Use StringUtils Methods",
-            "url": "rules/string-utils.html"
-        },
-        "TryWithResource": {
-            "name": "Use Try-With-Resource",
-            "url": "rules/use-try-with-resource.html"
-        },
-        "UseIsEmptyOnCollections": {
-            "name": "Replace Equality Check with isEmpty()",
-            "url": "rules/replace-equality-check-with-is-empty.html"
-        },
-        "UseStringBuilderAppend": {
-            "name": "Use StringBuilder::append",
-            "url": "rules/use-stringbuilder-append.html"
-        },
-        "WhileToForEach": {
-            "name": "Replace While-Loop with Enhanced For-Loop",
-            "url": "rules/while-to-for-each.html"
-        }
+module.exports = {
+    "ArithmethicAssignment": {
+        "name": "Replace Assignment with Compound Operator",
+        "url": "rules/arithmethic-assignment.html"
+    },
+    "BracketsToControl": {
+        "name": "Add Parenthesis to Control Statements",
+        "url": "rules/brackets-to-control.html"
+    },
+    "CodeFormatter": {
+        "name": "Reformat Code",
+        "url": "rules/code-formatter.html"
+    },
+    "CollectionRemoveAll": {
+        "name": "Replace removeAll() with clear()",
+        "url": "rules/collection-remove-all.html"
+    },
+    "DateDeprecated": {
+        "name": "Remove Deprecated Date Constructs",
+        "url": "rules/date-deprecated.html"
+    },
+    "DiamondOperator": {
+        "name": "Remove Explicit Type Argument",
+        "url": "rules/diamond-operator.html"
+    },
+    "EnhancedForLoopToStreamAnyMatch": {
+        "name": "Replace For-Loop with Stream::anyMatch",
+        "url": "rules/enhanced-for-loop-to-stream-any-match.html"
+    },
+    "EnhancedForLoopToStreamFindFirst": {
+        "name": "Replace For-Loop with Stream::findFirst",
+        "url": "rules/enhanced-for-loop-to-stream-find-first.html"
+    },
+    "EnhancedForLoopToStreamForEach": {
+        "name": "Replace For-Loop with Stream::forEach",
+        "url": "rules/enhanced-for-loop-to-stream-for-each.html"
+    },
+    "EnhancedForLoopToStreamSum": {
+        "name": "Replace For-Loop with Stream::sum",
+        "url": "rules/enhanced-for-loop-to-stream-sum.html"
+    },
+    "EnumsWithoutEquals": {
+        "name": "Replace equals() on Enum Constants",
+        "url": "rules/enums-without-equals.html"
+    },
+    "FieldRenaming": {
+        "name": "Rename fields",
+        "url": "rules/field-renaming.html"
+    },
+    "FlatMapInsteadOfNestedLoops": {
+        "name": "Replace Nested Loops with flatMap",
+        "url": "rules/flat-map-instead-of-nested-loops.html"
+    },
+    "ForToForEach": {
+        "name": "Replace For-Loop with Enhanced-For-Loop",
+        "url": "rules/for-to-for-each.html"
+    },
+    "FunctionalInterface": {
+        "name": "Use Functional Interfaces",
+        "url": "rules/functional-interface.html"
+    },
+    "GuardCondition": {
+        "name": "Use Guard Condition",
+        "url": "rules/guard-condition.html"
+    },
+    "ImmutableStaticFinalCollections": {
+        "name": "Replace static final Collections with Collections.unmodifiable...()",
+        "url": "rules/immutable-static-final-collections.html"
+    },
+    "IndexOfToContains": {
+        "name": "Replace indexOf() with contains()",
+        "url": "rules/index-of-to-contains.html"
+    },
+    "InefficientConstructor": {
+        "name": "Replace Inefficient Constructors with valueOf()",
+        "url": "rules/inefficient-constructor.html"
+    },
+    "LambdaForEachCollect": {
+        "name": "Use Stream::collect",
+        "url": "rules/lambda-for-each-collect.html"
+    },
+    "LambdaForEachIfWrapperToFilter": {
+        "name": "Use Stream::filter",
+        "url": "rules/lambda-for-each-if-wrapper-to-filter.html"
+    },
+    "LambdaForEachMap": {
+        "name": "Use Stream::map",
+        "url": "rules/lambda-for-each-map.html"
+    },
+    "LambdaToMethodReference": {
+        "name": "Replace Expression Lambda with Method Reference",
+        "url": "rules/lambda-to-method-reference.html"
+    },
+    "LocalVariableTypeInference": {
+        "name": "Use Local Variable Type Inference",
+        "url": "rules/local-variable-type-inference.html"
+    },
+    "MultiCatch": {
+        "name": "Use Multi-Catch",
+        "url": "rules/multi-catch.html"
+    },
+    "MultiVariableDeclarationLine": {
+        "name": "Split Multiple Variable Declarations",
+        "url": "rules/multi-variable-declaration-line.html"
+    },
+    "OptionalIfPresent": {
+        "name": "Use Optional::ifPresent",
+        "url": "rules/use-optional-if-present.html"
+    },
+    "OrganiseImports": {
+        "name": "Organize Imports",
+        "url": "rules/organise-imports.html"
+    },
+    "OverrideAnnotation": {
+        "name": "Use @Override Annotation",
+        "url": "rules/override-annotation.html"
+    },
+    "PrimitiveBoxedForString": {
+        "name": "Remove Boxing for String Conversions",
+        "url": "rules/primitive-boxed-for-string.html"
+    },
+    "PrimitiveObjectUseEquals": {
+        "name": "Use equals() on Primitive Objects",
+        "url": "rules/primitive-object-use-equals.html"
+    },
+    "PutIfAbsent": {
+        "name": "Replace put(..) with putIfAbsent(..)",
+        "url": "rules/put-if-absent.html"
+    },
+    "RearrangeClassMembers": {
+        "name": "Rearrange Class Members",
+        "url": "rules/rearrange-class-members.html"
+    },
+    "ReImplementingInterface": {
+        "name": "Remove Inherited Interfaces from Class Declaration",
+        "url": "rules/re-implementing-interface.html"
+    },
+    "RemoveDoubleNegationRule": {
+        "name": "Remove Double Negations",
+        "url": "rules/remove-double-negation.html"
+    },
+    "RemoveEmptyStatement": {
+        "name": "Remove Unnecessary Semicolons",
+        "url": "rules/remove-empty-statement.html"
+    },
+    "RemoveExplicitCallToSuper": {
+        "name": "Remove Explicit Call To super()",
+        "url": "rules/remove-explicit-call-to-super.html"
+    },
+    "RemoveNewStringConstructor": {
+        "name": "Use String Literals",
+        "url": "rules/remove-new-string-constructor.html"
+    },
+    "RemoveToStringOnString": {
+        "name": "Remove toString() on String",
+        "url": "rules/remove-to-string-on-string.html"
+    },
+    "RemoveUnnecessaryThrows": {
+        "name": "Remove Unnecessary Thrown Exceptions on Method Signatures",
+        "url": "rules/remove-unnecessary-throws.html"
+    },
+    "SerialVersionUid": {
+        "name": "Make SerialVersionUID Static Final",
+        "url": "rules/serial-version-uid.html"
+    },
+    "StandardLogger": {
+        "name": "System Out To Logging",
+        "url": "rules/standard-logger.html"
+    },
+    "StatementLambdaToExpression": {
+        "name": "Remove Lambda Expression Parenthesis",
+        "url": "rules/statement-lambda-to-expression.html"
+    },
+    "StringBufferToBuilder": {
+        "name": "StringBuffer() to StringBuilder()",
+        "url": "rules/string-buffer-to-builder.html"
+    },
+    "StringBuildingLoop": {
+        "name": "Replace For-Loop with Stream::collect(Collectors.joining())",
+        "url": "rules/string-building-loop.html"
+    },
+    "StringConcatToPlus": {
+        "name": "Replace Concatenation with Infix-Operation",
+        "url": "rules/string-concat-to-plus.html"
+    },
+    "StringFormatLineSeparator": {
+        "name": "Use Portable Newline",
+        "url": "rules/string-format-line-separator.html"
+    },
+    "StringLiteralEqualityCheck": {
+        "name": "Reorder String Equality Check",
+        "url": "rules/string-literal-equality-check.html"
+    },
+    "StringUtils": {
+        "name": "Use StringUtils Methods",
+        "url": "rules/string-utils.html"
+    },
+    "TryWithResource": {
+        "name": "Use Try-With-Resource",
+        "url": "rules/try-with-resource.html"
+    },
+    "UseIsEmptyOnCollections": {
+        "name": "Replace Equality Check with isEmpty()",
+        "url": "rules/use-is-empty-on-collections.html"
+    },
+    "UseStringBuilderAppend": {
+        "name": "Use StringBuilder::append",
+        "url": "rules/use-string-builder-append.html"
+    },
+    "WhileToForEach": {
+        "name": "Replace While-Loop with Enhanced For-Loop",
+        "url": "rules/while-to-for-each.html"
     }
+}
