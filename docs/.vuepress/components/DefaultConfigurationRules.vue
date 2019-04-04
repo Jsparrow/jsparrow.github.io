6ef7fdf85e4176c5ebe411eadb8165ed879439ba
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-04-04 17:02:42 +0200

    SIM-1512 ADD vue template for default configuration rules

---
+++ docs/.vuepress/components/DefaultConfigurationRules.vue
@@ -0,0 +1,22 @@
+<template>
+<ul>
+    <div v-for="rule in rules">
+        <li>
+            <router-link :to="rule.path">{{ rule.frontmatter.title }}</router-link>
+        </li>
+    </div>
+</ul>
+</template>
+
+<script>
+export default {
+    computed: {
+        rules() {
+            return this.$site.pages
+                .filter(x => x.path.startsWith('/rules/') && !x.frontmatter.rules_index && x.frontmatter.defaultConfiguration)
+                .sort((a, b) => b.frontmatter.title < a.frontmatter.title);
+        } 
+    }
+}
+</script>
+
