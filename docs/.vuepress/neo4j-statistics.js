f50c14cd6af525a3f20d8bf986eca466eb53753d
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-06-24 13:48:18 +0200

    SIM-1583 ADD statistics for neo4j

---
+++ docs/.vuepress/neo4j-statistics.js
@@ -0,0 +1,254 @@
+module.exports = [
+  {
+    projectName: "neo4j",
+    timestampGitHubStart: 1561369163,
+    timestampJSparrowFinish: 1561369529,
+    totalFilesChanged: 5633,
+    totalFilesCount: 5693,
+    totalIssuesFixed: 11634,
+    totalTimeSaved: 38845,
+    rules: [
+      {
+        fileCount: 48,
+        issuesFixedCount: 54,
+        remediationCost: 5,
+        ruleId: "CollapseIfStatements"
+      },
+      {
+        fileCount: 12,
+        issuesFixedCount: 21,
+        remediationCost: 5,
+        ruleId: "RemoveUnusedParameter"
+      },
+      {
+        fileCount: 405,
+        issuesFixedCount: 546,
+        remediationCost: 5,
+        ruleId: "GuardCondition"
+      },
+      {
+        fileCount: 1,
+        issuesFixedCount: 1,
+        remediationCost: 15,
+        ruleId: "LambdaForEachCollect"
+      },
+      {
+        fileCount: 11,
+        issuesFixedCount: 12,
+        remediationCost: 2,
+        ruleId: "UseIsEmptyOnCollections"
+      },
+      {
+        fileCount: 55,
+        issuesFixedCount: 101,
+        remediationCost: 10,
+        ruleId: "StringLiteralEqualityCheck"
+      },
+      {
+        fileCount: 1,
+        issuesFixedCount: 1,
+        remediationCost: 2,
+        ruleId: "PutIfAbsent"
+      },
+      {
+        fileCount: 12,
+        issuesFixedCount: 13,
+        remediationCost: 15,
+        ruleId: "LambdaForEachMap"
+      },
+      {
+        fileCount: 2,
+        issuesFixedCount: 15,
+        remediationCost: 5,
+        ruleId: "InefficientConstructor"
+      },
+      {
+        fileCount: 10,
+        issuesFixedCount: 15,
+        remediationCost: 5,
+        ruleId: "ForToForEach"
+      },
+      {
+        fileCount: 22,
+        issuesFixedCount: 55,
+        remediationCost: 2,
+        ruleId: "MultiVariableDeclarationLine"
+      },
+      {
+        fileCount: 3,
+        issuesFixedCount: 3,
+        remediationCost: 5,
+        ruleId: "BufferedReaderLines"
+      },
+      {
+        fileCount: 114,
+        issuesFixedCount: 302,
+        remediationCost: 2,
+        ruleId: "RemoveUnnecessaryThrows"
+      },
+      {
+        fileCount: 21,
+        issuesFixedCount: 25,
+        remediationCost: 1,
+        ruleId: "RemoveExplicitCallToSuper"
+      },
+      {
+        fileCount: 3,
+        issuesFixedCount: 3,
+        remediationCost: 5,
+        ruleId: "WhileToForEach"
+      },
+      {
+        fileCount: 13,
+        issuesFixedCount: 38,
+        remediationCost: 15,
+        ruleId: "TryWithResource"
+      },
+      {
+        fileCount: 2,
+        issuesFixedCount: 2,
+        remediationCost: 5,
+        ruleId: "MultiCatch"
+      },
+      {
+        fileCount: 283,
+        issuesFixedCount: 486,
+        remediationCost: 15,
+        ruleId: "EnhancedForLoopToStreamForEach"
+      },
+      {
+        fileCount: 1,
+        issuesFixedCount: 1,
+        remediationCost: 10,
+        ruleId: "StandardLogger"
+      },
+      {
+        fileCount: 63,
+        issuesFixedCount: 562,
+        remediationCost: 15,
+        ruleId: "FieldRenaming"
+      },
+      {
+        fileCount: 7,
+        issuesFixedCount: 7,
+        remediationCost: 2,
+        ruleId: "EnhancedForLoopToStreamFindFirst"
+      },
+      {
+        fileCount: 225,
+        issuesFixedCount: 346,
+        remediationCost: 5,
+        ruleId: "StatementLambdaToExpression"
+      },
+      {
+        fileCount: 6,
+        issuesFixedCount: 9,
+        remediationCost: 2,
+        ruleId: "ArithmethicAssignment"
+      },
+      {
+        fileCount: 8,
+        issuesFixedCount: 8,
+        remediationCost: 5,
+        ruleId: "FunctionalInterface"
+      },
+      {
+        fileCount: 6,
+        issuesFixedCount: 8,
+        remediationCost: 10,
+        ruleId: "ImmutableStaticFinalCollections"
+      },
+      {
+        fileCount: 4,
+        issuesFixedCount: 14,
+        remediationCost: 5,
+        ruleId: "RemoveNewStringConstructor"
+      },
+      {
+        fileCount: 853,
+        issuesFixedCount: 908,
+        remediationCost: 5,
+        ruleId: "RearrangeClassMembers"
+      },
+      {
+        fileCount: 17,
+        issuesFixedCount: 26,
+        remediationCost: 2,
+        ruleId: "EnumsWithoutEquals"
+      },
+      {
+        fileCount: 14,
+        issuesFixedCount: 18,
+        remediationCost: 5,
+        ruleId: "OverrideAnnotation"
+      },
+      {
+        fileCount: 5633,
+        issuesFixedCount: 5633,
+        remediationCost: 1,
+        ruleId: "CodeFormatter"
+      },
+      {
+        fileCount: 882,
+        issuesFixedCount: 2185,
+        remediationCost: 2,
+        ruleId: "UseStringBuilderAppend"
+      },
+      {
+        fileCount: 6,
+        issuesFixedCount: 9,
+        remediationCost: 1,
+        ruleId: "StringFormatLineSeparator"
+      },
+      {
+        fileCount: 83,
+        issuesFixedCount: 110,
+        remediationCost: 2,
+        ruleId: "LambdaToMethodReference"
+      },
+      {
+        fileCount: 5,
+        issuesFixedCount: 5,
+        remediationCost: 2,
+        ruleId: "ReImplementingInterface"
+      },
+      {
+        fileCount: 10,
+        issuesFixedCount: 10,
+        remediationCost: 2,
+        ruleId: "EnhancedForLoopToStreamAnyMatch"
+      },
+      {
+        fileCount: 7,
+        issuesFixedCount: 8,
+        remediationCost: 1,
+        ruleId: "DiamondOperator"
+      },
+      {
+        fileCount: 28,
+        issuesFixedCount: 43,
+        remediationCost: 5,
+        ruleId: "PrimitiveBoxedForString"
+      },
+      {
+        fileCount: 1,
+        issuesFixedCount: 1,
+        remediationCost: 2,
+        ruleId: "IndexOfToContains"
+      },
+      {
+        fileCount: 21,
+        issuesFixedCount: 28,
+        remediationCost: 5,
+        ruleId: "LambdaForEachIfWrapperToFilter"
+      },
+      {
+        fileCount: 2,
+        issuesFixedCount: 2,
+        remediationCost: 2,
+        ruleId: "PrimitiveObjectUseEquals"
+      }
+    ]
+  }
+
+]
