b58fa8cf305a4097de01a8728934d7dd3b6cd5d7
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2019-08-22 15:36:03 +0200

    SIM-1622 ADD buy link that always stays in the right bottom corner

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,17 +1,20 @@
 <template>
   <ParentLayout>
     <PageFooter slot="page-bottom" />
+    <BuyLink slot="page-bottom" />
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from "@parent-theme/layouts/Layout.vue";
 import PageFooter from "@theme/components/PageFooter.vue";
+import BuyLink from "@theme/components/BuyLink.vue";
 
 export default {
   components: {
     ParentLayout,
-    PageFooter
+    PageFooter,
+    BuyLink
   }
 };
 </script>
