8a9274d5671f2c70116e0123f0ce81677d3da2a4
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2019-09-09 18:22:17 +0200

    SIM-1629 ADD version notice headline to put on each rule

---
+++ docs/.vuepress/components/VersionNotice.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Automatic Application of This Rule</h2>
+    <div v-if="$page.frontmatter.tags.includes('Free')">
+      The automatic application of this rule is supported in the following jSparrow versions:
+      <ul>
+        <li><a href="/eclipse/starter-registration.html">jSparrow Starter version</a></li>
+        <li><a href="https://order.shareit.com/cart/new?vendorid=200267175&PRODUCT%5B300770603%5D=1">jSparrow Pro version</a></li>
+      </ul>
+    </div>
+    <div v-else>
+      The automatic application of this rule is supported in the following jSparrow version:
+      <ul>
+        <li><a href="https://order.shareit.com/cart/new?vendorid=200267175&PRODUCT%5B300770603%5D=1">jSparrow Pro version</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
