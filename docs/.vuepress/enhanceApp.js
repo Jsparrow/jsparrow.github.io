627c1b19fc78d9a273dafbca6d27c0ee51b046d3
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-09-10 12:15:18 +0200

    SIM-1630 UPDATE add redirects for removed urls

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,19 @@
 import Vuetify from 'vuetify';
 
 export default ({
-    Vue, // the version of Vue being used in the VuePress app
-    options, // the options for the root Vue instance
-    router, // the router instance for the app
-    siteData // site metadata
-  }) => {
-    Vue.use(Vuetify);
-  }
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData // site metadata
+}) => {
+  Vue.use(Vuetify);
+
+  // routes for redirecting to the correct page after getting rid of /github/
+  router.addRoutes([
+    { path: '/github/statistics.html', redirect: '/statistics/statistics.html' },
+    { path: '/github/mdm-statistics.html', redirect: '/statistics/mdm-statistics.html' },
+    { path: '/github/jenkins-statistics.html', redirect: '/statistics/jenkins-statistics.html' },
+    { path: '/github/neo4j-statistics.html', redirect: '/statistics/neo4j-statistics.html' },
+    { path: '/github/', redirect: '/cloud/getting-started.html' }
+  ])
+}
