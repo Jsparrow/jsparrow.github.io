e309c774b9e53de9da4fb6db324b096c6290d436
Ludwig Werzowa <luigi.werzowa@gmail.com>
2021-03-10 16:48:20 +0100

    SIM-1913 UPDATE aggregated data in table, still side effects

--- docs/.vuepress/components/StatisticsSummaryAll.vue
+++ docs/.vuepress/components/StatisticsSummaryAll.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <h2>Testing ground</h2>
+    <h2>Testing Ground Start</h2>
     <div>
-      <ul>
-        <li v-for="n in aggregatedRules">{{ n }}</li>
-      </ul>
-      <p />
-      {{ statistics }}
-      <p />
+      <v-data-table
+        :headers="headers"
+        :items="aggregatedRules"
+        class=""
+        hide-actions=""
+        :custom-sort="customSort"
+        :must-sort="true"
+      >
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-right">
+            <a v-bind:href="findRuleLink(props.item.ruleId)" target="_blank">
+              {{ findRuleName(props.item.ruleId) }}
+            </a>
+          </td>
+          <td class="text-xs-right">{{ props.item.issuesFixedCount }}</td>
+          <td class="text-xs-right">{{ props.item.fileCount }}</td>
+          <td class="text-xs-right">
+            {{
+              secondsToHms(
+                props.item.remediationCost * props.item.issuesFixedCount * 60
+              )
+            }}
+          </td>
+        </template>
+      </v-data-table>
     </div>
+    <h2>Testing Ground End</h2>
   </div>
 </template>
 
@@ -17,41 +37,58 @@ export default {
   data() {
     return {
       statistics: require("../statistics.js"),
+      headers: [
+        { text: "Rule Name", value: "ruleId", align: "center" },
+        { text: "Issues Fixed", value: "issuesFixedCount", align: "center" },
+        { text: "Files Changed", value: "fileCount", align: "center" },
+        { text: "Time saved", value: "remediationCost", align: "center" },
+      ],
+      ruleCache: {},
     };
   },
 
   computed: {
     aggregatedRules: function () {
-      var uniqueRules = []
-      console.log(this.statistics)
+      var uniqueRules = [];
+      console.log(this.statistics);
 
       for (var i = 0; i < this.statistics.length; i++) {
-        console.log(this.statistics[i].rules.length)
+        console.log(this.statistics[i].rules.length);
         for (var j = 0; j < this.statistics[i].rules.length; j++) {
-          const currentObj = this.statistics[i].rules[j];
-          console.log(currentObj.ruleId)
-          const objIndex = uniqueRules.findIndex((x => x.ruleId == currentObj.ruleId));
-          if (objIndex === -1) {
-          	uniqueRules.push(currentObj);
-          	console.log("new rule: " + currentObj.ruleId);
+          const currentRule = this.statistics[i].rules[j];
+          console.log(currentRule.ruleId);
+          const ruleIndex = uniqueRules.findIndex(
+            (x) => x.ruleId == currentRule.ruleId
+          );
+          if (ruleIndex === -1) {
+            uniqueRules.push(currentRule);
+            console.log("new rule: " + currentRule.ruleId);
           } else {
-          	console.log("ooooold stuff: " + currentObj.ruleId);
-            console.log("old: ")
-            console.log(uniqueRules[objIndex])
-            console.log("adding: ")
-            console.log(currentObj)
-            uniqueRules[objIndex].fileCount += currentObj.fileCount;
-            uniqueRules[objIndex].issuesFixedCount += currentObj.issuesFixedCount;
-            uniqueRules[objIndex].remediationCost += currentObj.remediationCost;
-            console.log("new: ")
-            console.log(uniqueRules[objIndex])
+            console.log("ooooold stuff: " + currentRule.ruleId);
+            console.log("old: ");
+            console.log(uniqueRules[ruleIndex]);
+            console.log("adding: ");
+            console.log(currentRule);
+            uniqueRules[ruleIndex].fileCount += currentRule.fileCount;
+            uniqueRules[ruleIndex].issuesFixedCount +=
+              currentRule.issuesFixedCount;
+            uniqueRules[ruleIndex].remediationCost +=
+              currentRule.remediationCost;
+            console.log("new: ");
+            console.log(uniqueRules[ruleIndex]);
           }
-          
-          console.log(uniqueRules)
+
+          console.log(uniqueRules);
         }
       }
-      return uniqueRules
-    }
+      return uniqueRules;
+    },
+
+    rules() {
+      return this.$site.pages.filter(
+        (x) => x.path.startsWith("/rules/") && !x.frontmatter.rules_index
+      );
+    },
   },
 
   methods: {
@@ -96,25 +133,92 @@ export default {
         totalRunTime = 0;
       for (i = 0; i < data.length; i++) {
         //console.log(data[i].timestampJSparrowFinish - data[i].timestampGitHubStart)
-        totalRunTime += data[i].timestampJSparrowFinish - data[i].timestampGitHubStart;
+        totalRunTime +=
+          data[i].timestampJSparrowFinish - data[i].timestampGitHubStart;
       }
       return totalRunTime;
     },
 
-    secondsToHms: function(d) {
+    secondsToHms: function (d) {
       d = Number(d);
       var h = Math.floor(d / 3600);
-      var m = Math.floor(d % 3600 / 60);
-      var s = Math.floor(d % 3600 % 60);
+      var m = Math.floor((d % 3600) / 60);
+      var s = Math.floor((d % 3600) % 60);
 
       // always show minutes, add comma and seconds if they are not 0
       var hDisplay = h > 0 ? h + (h == 1 ? " hour" : " hours") : "";
       var mDisplay = m > 0 ? m + (m == 1 ? " minute" : " minutes") : "";
       var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
 
-      var text = [hDisplay, mDisplay, sDisplay].filter(Boolean).join(", ")
+      var text = [hDisplay, mDisplay, sDisplay].filter(Boolean).join(", ");
       return text;
-    }
-  }
+    },
+
+    findRuleName: function (id) {
+      var rule = this.findRule(id);
+      return rule.frontmatter.title;
+    },
+
+    findRuleLink: function (id) {
+      var rule = this.findRule(id);
+      return rule.path;
+    },
+
+    findRule: function (id) {
+      if (id in this.ruleCache) {
+        return this.ruleCache[id];
+      }
+
+      for (var i = 0; i < this.rules.length; i++) {
+        var element = this.rules[i];
+        if (element.frontmatter.ruleId == id) {
+          this.ruleCache[id] = element;
+          return element;
+        }
+      }
+    },
+
+    customSort: function (items, index, isDescending) {
+      items.sort((a, b) => {
+        if (index === "ruleId") {
+          if (isDescending) {
+            return this.findRuleName(b.ruleId).localeCompare(
+              this.findRuleName(a.ruleId)
+            );
+          } else {
+            return this.findRuleName(a.ruleId).localeCompare(
+              this.findRuleName(b.ruleId)
+            );
+          }
+        } else if (index === "issuesFixedCount") {
+          if (isDescending) {
+            return a.issuesFixedCount - b.issuesFixedCount;
+          } else {
+            return b.issuesFixedCount - a.issuesFixedCount;
+          }
+        } else if (index === "fileCount") {
+          if (isDescending) {
+            return a.fileCount - b.fileCount;
+          } else {
+            return b.fileCount - a.fileCount;
+          }
+        } else if (index === "remediationCost") {
+          if (isDescending) {
+            return (
+              a.remediationCost * a.issuesFixedCount -
+              b.remediationCost * b.issuesFixedCount
+            );
+          } else {
+            return (
+              b.remediationCost * b.issuesFixedCount -
+              a.remediationCost * a.issuesFixedCount
+            );
+          }
+        }
+      });
+
+      return items;
+    },
+  },
 };
 </script>
