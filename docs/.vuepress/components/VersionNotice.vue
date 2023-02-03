2f5816898cc263b8ac387f6cebe85913035a0ee6
Sasithon Prehofer <sasithon.prehofer@splendit.at>
2023-02-03 12:00:06 +0100

    UPDATE call-to-action

--- docs/.vuepress/components/VersionNotice.vue
+++ docs/.vuepress/components/VersionNotice.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
-    <h2>You Want To Have Those Changes Done Automatically?</h2>
+    <h2>Use a Java Refactoring Tool</h2>
+    
     <div v-if="$page.frontmatter.tags.includes('Free')">
-      The automatic application of this rule is supported in the following jSparrow versions:
-      <ul>
-        <li><a href="/eclipse/starter-registration.html">jSparrow Starter</a></li>
-        <li><a href="https://jsparrow.io/get-jsparrow/">jSparrow Basic</a></li>
-        <li><a href="https://jsparrow.io/contact">jSparrow Enterprise</a></li>
-      </ul>
+    <h3>Automate this Refactoring system-wide</h3>   
+    <p>
+      You can apply this refactoring for <em>free</em> with the jSparrow Eclipse IDE plug-in.
+      <br>
+        Install the plug-in for Eclipse IDE: <a href="https://marketplace.eclipse.org/content/jsparrow" target="_blank" rel="noopener noreferrer">Eclipse Marketplace</a>.
+      </p>
     </div>
     <div v-else>
-      The automatic application of this rule is supported in the following jSparrow version:
-      <ul>
-        <li><a href="https://jsparrow.io/get-jsparrow/">jSparrow Basic</a></li>
-        <li><a href="https://jsparrow.io/contact">jSparrow Enterprise</a></li>
-      </ul>
+    <h3>No license required</h3>   
+      <p>
+        You can <em>review</em> this refactoring on your code <strong>without a license</strong> by installing jSparrow to your Eclipse IDE.
+       Install the plug-in from Eclipse IDE: <a href="https://marketplace.eclipse.org/content/jsparrow" target="_blank" rel="noopener noreferrer">Eclipse Marketplace</a>.
+      </p>
+      <h3>System-wide Refactoring</h3>
+      <p>
+      <em>
+        Do you want to automate this refactoring (and many more) to your system-wide code?
+      </em>
+        The automatic application of this system-wide refactoring can be unlocked by acquiring 
+        <strong><a href="https://jsparrow.io/pricing/" target="_blank" rel="noopener noreferrer">your jSparrow license</a></strong>.
+      </p>   
     </div>
   </div>
 </template>
